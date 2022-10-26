66e8a59c12981e32eb41a5101765a3a09915f7df
Alan Sebastián Arce <104936228+AlanSebastianArce@users.noreply.github.com>
2022-10-26 18:23:50 -0300

    create sin terminar (trabajar) - CRUD 80%

--- save_products.php
+++ save_products.php
@@ -1,6 +1,30 @@
 <?php
-$product=$_POST['producto'];
-$price=$_POST['precio'];
-$category=$_POST['categoria'];
+$product = $_POST['producto'];
+$price = $_POST['precio'];
+$category = $_POST['categoria'];
 
-echo $product." ".$price." ".$category;
+/** echo $product." ".$price." ".$category; **/
+
+include_once("config_products.php");
+include_once("db.php");
+$link = new Db();
+include_once('upload_image.php');
+$path_img = $directorio . $nombre_img;   //Ruta completa de la imagen levantada.
+$sql = "insert products(product_name, image, price, id_category) values (:product,:path_img,:price,:category)";
+$stmt = $link->prepare($sql);
+$stmt->bindValue(':product', $product);
+$stmt->bindValue(':price', $price);
+$stmt->bindValue(':category', $category);
+$stmt->bindValue(':path_img', $path_img);  //No es necesario sanear
+
+if($stmt->execute()){
+    ?>
+    <script>
+    alert("Producto Ingresado!");
+    window.location="insert_products.php";
+    </script>
+    <?php
+}
+
+//$stmt->execute();
+//$data = $stmt->fetchAll();
--- welcome.php
+++ welcome.php
@@ -1,99 +1,115 @@
-
-
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link
-      rel="stylesheet"
-      href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css"
-    />
-    
-    <script src="js/jquery-3.6.1.min.js"></script>
-    <script src="js/bootbox.min.js"></script>
-    <script> function deleteProduct(cod){
-      bootbox.confirm("Desea usted Eliminar el producto "+cod, function(result){
-        if(result){
-          window.location="delete.php?q="+cod;
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" />
+
+  <script src="js/jquery-3.6.1.min.js"></script>
+  <script src="js/bootbox.min.js"></script>
+  <script>
+    function deleteProduct(cod) {
+      bootbox.confirm("Desea usted Eliminar el producto " + cod, function(result) {
+        if (result) {
+          window.location = "delete.php?q=" + cod;
         }
       });
     }
-    </script>
 
-    <title>Document</title>
+    function updateProduct(cod) {
+      window.location = "edit.php?q=" + cod
+    }
+  </script>
+
+  <title>Mi Cuenta</title>
 </head>
+
 <body>
-<?php
+  <?php
 
-session_start();
-if($_SESSION['logueado'])
-{
-echo "Bienvenido/a ".$_SESSION['username'];
-echo "<br>";
-echo "Horario de conexion".$_SESSION['time'];
-echo "<br>";
-echo "<a href='logout.php'>logout</a>";
-echo "<a href='insert_products.php' class='btn btn-primary'> Ingresar Producto </a>";
-include_once("config_products.php");
-include_once("db.php");
-$link = new Db();
-$sql = "select p.id_product, p.product_name, p.price, date_format(p.start_date, '%d/%m/%Y') as date, c.category_name from products p inner join categories c on c.id_category=p.id_category order by c.category_name asc";
-$stmt = $link->prepare($sql);
-$stmt->execute();
-$data=$stmt->fetchAll();
+  session_start();
+  if ($_SESSION['logueado']) {
+    echo "Bienvenido/a " . $_SESSION['username'];
+    echo "<br>";
+    echo "Horario de conexion" . $_SESSION['time'];
+    echo "<br>";
+    echo "<a href='logout.php'>logout</a>";
+    echo "<a href='insert_products.php' class='btn btn-primary'> Ingresar Producto </a>";
+    echo "<a href='insert_categories.php' class='btn btn-primary'> Ingresar Categoria </a>";
+    include_once("config_products.php");
+    include_once("db.php");
+    $link = new Db();
+    $sql = "select p.id_product, p.product_name, p.price, date_format(p.start_date, '%d/%m/%Y') as date, c.category_name from products p inner join categories c on c.id_category=p.id_category order by c.category_name asc";
+    $stmt = $link->prepare($sql);
+    $stmt->execute();
+    $data = $stmt->fetchAll();
 
-$table="<table class='table table-bordered table-striped'> <thead class='thead-dark'> <tr> <th> Id </th> <th> Producto </th> <th> Categoria </th> <th> Precio </th> <th> Fecha de Alta </th> <th> Eliminar </th> <th> Actualizar </th> </tr> </thead> <tbody>";
-echo $table;
+    $table = "<table class='table table-bordered table-striped'> <thead class='thead-dark'> <tr> <th> Id </th> <th> Producto </th> <th> Categoria </th> <th> Precio </th> <th> Fecha de Alta </th> <th> Eliminar </th> <th> Actualizar </th> </tr> </thead> <tbody>";
+    echo $table;
 
 
-  foreach($data as $row){
-?>
-<tr>
-  <td>
-    <?php
-    echo $row['id_product'];
-    ?>
-    </td>
-    <td>
-    <?php
-    echo $row['product_name'];
-    ?>
-    </td>
-    <td>
-    <?php
-    echo $row['category_name'];
-    ?>
-    </td>
-    <td>
-    <?php
-    echo $row['price'];
-    ?>
-    </td>
-    <td>
-    <?php
-    echo $row['date'];
-    ?>
-    </td>
-    
-    <td>
-      <a href="#" onclick="deleteProduct(<?php echo $row['id_product'] ?>)"> Eliminar Producto </a>
-  </td>
+    foreach ($data as $row) {
+  ?>
+      <tr>
+        <td>
+          <?php
+          echo $row['id_product'];
+          ?>
+        </td>
+        <td>
+          <?php
+          echo $row['product_name'];
+          ?>
+        </td>
+        <td>
+          <?php
+          echo $row['category_name'];
+          ?>
+        </td>
+        <td>
+          <?php
+          echo $row['price'];
+          ?>
+        </td>
+        <td>
+          <?php
+          echo $row['date'];
+          ?>
+        </td>
 
-  <td>
-      <a href="#" onclick="updateProduct(<?php echo $row['id_product'] ?>)"> Actualizar Producto </a>
-  </td>
-  </tr>
-<?php
-}
-echo "</tbody>";
-echo "</table>";
-}
-else
-{header('location:login.html');}
+        <td>
+          <a href="#" onclick="deleteProduct(<?php echo $row['id_product'] ?>)"> Eliminar Producto </a>
+        </td>
 
-?>
+        <td>
+          <a href="#" onclick="updateProduct(<?php echo $row['id_product'] ?>)"> Actualizar Producto </a>
+        </td>
+      </tr>
+  <?php
+    }
+    echo "</tbody>";
+    echo "</table>";
+  } else {
+    header('location:login.html');
+  }
+
+  ?>
+
+  <br>
+  <br>
+  <br>
+
+  <?php
+  $sqlCtg = "select id_category, category_name from categories order by id_category asc";
+  $stmt = $link->prepare($sqlCtg);
+  $stmt->execute();
+  $dataCtg = $stmt->fetchAll();  
+
+  k
+  ?>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
 </body>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
+
 </html>
